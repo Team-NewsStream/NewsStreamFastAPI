c5115ea0c1835c5ed49c0beafb00758aa578c26b
Parul Chambiyal <parulchambial@gmail.com>
2025-04-02 10:45:04 +0530

    feat(auth): Implement user registration, authentication with JWT.

---
+++ schemas/user.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    password: str
+
+    class Config:
+        from_attributes = True
+
+
+class UserResponse(BaseModel):
+    name: str
+    email: str
+
+    class Config:
+        from_attributes = True
+
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str
+
+
+class TokenResponse(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
